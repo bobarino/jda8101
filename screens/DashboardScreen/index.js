b133612b709d4602fa86b9d65e2a32d2bc35cec6
Nick Giammanco <ngiammanco@gmail.com>
2018-04-10 22:08:10 -0400

    Initial Dashboard screen layout in progress. Need workout screen to create navigation for the button

--- screens/DashboardScreen/index.js
+++ screens/DashboardScreen/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button, Image } from 'react-native'
 
 import styles from './styles'
 
 export default () => (
   <View style={styles.container}>
-    <Text> This is a screen! </Text>
+    <Text style={styles.titleText}>Today{"\'"}s Workout</Text>
+    <Text style={styles.bodyText}>Relative Strength:{"\n"}Week A Wednesday</Text>
+    <View style={styles.buttonContainer}>
+      <Button
+        onPress={() => {
+
+        }}
+        title="Begin"
+      />
+    </View>
+    <Text style={styles.bodyText}>My Workout History:</Text>
+    <Image
+      source={require('../../assets/images/TRIMP.png')}
+    />
   </View>
 )
--- screens/DashboardScreen/styles.js
+++ screens/DashboardScreen/styles.js
@@ -3,7 +3,19 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
   },
+  titleText: {
+    flexDirection: 'column',
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  bodyText: {
+    flexDirection: 'column',
+    fontSize: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'column',
+    backgroundColor: 'blue',
+},
 })
