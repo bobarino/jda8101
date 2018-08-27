f9e96c6775975906a08fb3ad504071cdbd8c9d51
Alex O'Connell <oconnella19@gatech.edu>
2018-08-27 13:01:11 -0400

    remove changes to make it work with expo. now it builds natively in Xcode

--- App.js
+++ App.js
@@ -2,15 +2,8 @@ import React, {Component} from 'react';
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
 import { Auth } from './router';
 
-import { Font } from 'expo';
-
 
 export default class App extends Component {
-  componentDidMount() {
-    Font.loadAsync({
-      'Ionicons': require('./node_modules/react-native-vector-icons/Fonts/Ionicons.ttf'),
-    });
-  }
   render() {
     return (
         <Auth />
--- ios/userapp1/AppDelegate.m
+++ ios/userapp1/AppDelegate.m
@@ -16,7 +16,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-
   [FIRApp configure];
 
   NSURL *jsCodeLocation;
