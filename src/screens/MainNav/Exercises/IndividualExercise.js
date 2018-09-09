4edb6a0dcfe553780ecdfee4b3387d1bc02d4fd8
Alex O'Connell <oconnella19@gatech.edu>
2018-09-09 13:26:42 -0400

    Remove native-base from individual exercise page and add a header with navigation buttons

--- src/screens/MainNav/Exercises/IndividualExercise.js
+++ src/screens/MainNav/Exercises/IndividualExercise.js
@@ -1,35 +1,53 @@
 import React, { Component } from "react";
-import {
-  Container, Header, Content, Item, Input, Icon,
-  Button, List, ListItem, Text
-} from "native-base";
-import Exercises from "../../../entities/Exercises";
+
+import { StyleSheet, Text, View } from "react-native";
+import Button from "../../../components/Button";
+
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 
 export default class IndividualExercise extends Component {
   constructor(props) {
     super(props);
-
-    Exercises.getList().then((list) => this.setState({ exers: list, loading: false }));
-
-    this.state = {
-      exers: [],
-      loading: true,
-    };
   }
 
 
   render() {
     const exercise = this.props.navigation.state.params.exercise;
 
-
     return (
-      <Container>
-        <Header style={{ backgroundColor: "#324151" }} searchBar rounded>
-          <Text>{exercise.exName}</Text>
-        </Header>
-        <Content />
-      </Container>
+      <View style={{ width: "100%", height: "100%" }}>
+        <View style={{ height: 50 }}>
+          <View style={styles.header}>
+            <Button style={styles.backButton} onPress={() => this.props.navigation.navigate("ExercisesList")}>
+              <Ionicons name="md-arrow-round-back" size={35} color="#9599a2" />
+            </Button>
+            <Text style={styles.headerText}>{exercise.exName}</Text>
+            <View style={{ width: 35 }} />
+          </View>
+        </View>
+      </View >
     );
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#324151",
+    height: 50
+  },
+  backButton: {
+    paddingLeft: 10,
+    width: 35,
+    height: 35
+  },
+  headerText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center"
+  }
+});
