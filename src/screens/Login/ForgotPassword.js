b5e72af30904fc4ca7f7061d86fbb7f7ca9ac137
Johnny Lee <johnnylee@lawn-128-61-124-4.lawn.gatech.edu>
2018-09-09 17:18:12 -0400

    forgot password shell

--- src/screens/Login/ForgotPassword.js
+++ src/screens/Login/ForgotPassword.js
@@ -3,11 +3,35 @@ import { View, Text, StyleSheet } from "react-native";
 
 
 export default class ForgotPassword extends Component {
+    state = { email: "", errorMessage: "", showSpinner: false }
 
+    handleSignUp = () => {
+        this.setState({ showSpinner: true });
+        LoginService.sendPasswordResetEmail(email)
+        .then(() => {
+            this.setState({showSpinner: false });
+        })
+        .catch(error => this.setState({ errorMessage: error.message, showSpinner: false }));
+    }
   render() {
     return (
       <View style={styles.container}>
         <Text>Forgot Password</Text>
+      <Spinner show={this.state.showSpinner} />
+      <TextInput
+          placeholder="Email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          style={styles.textInput}
+          onChangeText={email => this.setState({ email })}
+          value={this.state.email}
+      />
+      <View style={{ width: "100%" }}>
+        <Button style={styles.registerButton}
+          onPress={this.sendReset}>
+        <Text style={styles.registerButtonText}>Send Password Reset Email ></Text>
+        </Button>
+      </View>
       </View>
     );
   }
