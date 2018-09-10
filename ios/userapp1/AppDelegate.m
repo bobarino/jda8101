1f62a9d4a071c68aaf8360ad0e3df281e3fc12a5
Alex O'Connell <oconnella19@gatech.edu>
2018-09-09 22:24:13 -0400

    Add last minute demo fixes
    Add library for visualizing touches on the screen during demos
    Add button for start workout on home screen that was missing

--- ios/userapp1/AppDelegate.m
+++ ios/userapp1/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <React/RCTRootView.h>
 
 #import <Firebase.h>
+#import "TouchVisualizer.h"
 
 @implementation AppDelegate
 
@@ -21,18 +22,20 @@
   NSURL *jsCodeLocation;
   
   // Use this for loading from the package
-  //  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
   
   // Use this for debugging and loading from the local packager
-   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"userapp1"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  // use this for normal use
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  // use this for demos to visualize touches
+  // self.window = [[TouchVisualizer alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
