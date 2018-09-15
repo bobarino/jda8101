a999d03a6b1976cdac7f93c82cdbaf84fc29046c
Alex O'Connell <oconnella19@gatech.edu>
2018-09-15 18:35:54 -0400

    Rewrite backend to reuse more code and be easier to edit.

--- src/screens/MainNav/Workout/Program.js
+++ src/screens/MainNav/Workout/Program.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Text, View, StyleSheet } from "react-native";
-import Programs from "../../../entities/Programs";
+import { Programs } from "../../../entities";
 import { getWorkoutDayAndWeek, dayStrings } from "../../../Utils";
 
 export default class Program
